68824fbe99da7b351bcb32e6fcb6cd27e2ca1a01
ingenarel <ingenarelitems@gmail.com>
2024-07-16 23:22:51 +0600

    tracker should now continue from before even when stopped. also made a good directory structure for the logs

--- tracker.py
+++ tracker.py
@@ -13,7 +13,7 @@ def log_parser(
             sessions[tracked_tasks]=[] # this is sessions list
             if tracked_tasks in currently_tracked_tasks:
                 # print("tracked_tasks in currently_tracked_tasks")
-                sessions[tracked_tasks].append({"starttime": time.asctime(), "endtime": None})
+                sessions[tracked_tasks].append({"starttime": time.strftime('%M-%S'), "endtime": None})
             else:
                 sessions[tracked_tasks].append({"starttime": None, "endtime": None})
 
@@ -25,37 +25,66 @@ def log_parser(
                 # print("tracked_tasks not in currently_tracked_tasks")
                 if sessions[tracked_tasks][-1]["starttime"] != None and sessions[tracked_tasks][-1]["endtime"] == None:
                     # print("sessions[tracked_tasks][-1]['endtime'] == None")
-                    sessions[tracked_tasks][-1]["endtime"] = time.asctime()
+                    sessions[tracked_tasks][-1]["endtime"] = time.strftime('%M-%S')
             else:
                 if sessions[tracked_tasks][-1]["starttime"] == None:
-                    sessions[tracked_tasks][-1]["starttime"] = time.asctime()
+                    sessions[tracked_tasks][-1]["starttime"] = time.strftime('%M-%S')
                 else:
                     if sessions[tracked_tasks][-1]["endtime"] != None:
                         # print("sessions[tracked_tasks][-1]['endtime'] != None:")
-                        sessions[tracked_tasks].append({"starttime": time.asctime(), "endtime": None})
+                        sessions[tracked_tasks].append({"starttime": time.strftime('%M-%S'), "endtime": None})
 
     return sessions 
 
 def main():
     tracked:list = ["nvim", "firefox", "yazi", "discord"]
-    write_delay:int = 60
+    write_delay:int = 10
     update_delay:int = 1
-    x = True
+    x = None
+    try:
+        with open(f"log/{time.strftime('%Y')}/{time.strftime('%j')}/{time.strftime('%H')}.log", "r") as load_log_file:
+            file_load = json.load(load_log_file)
+        x = "file"
+    except FileNotFoundError:
+        pass
+        
     while True:
         for _ in range(write_delay):
             currently_tracked = set(process.info["name"] for process in psutil.process_iter(["name"]) if process.info["name"] in tracked)
-            log = log_parser(
-                    list_of_tracked_tasks=tracked, 
-                    currently_tracked_tasks=currently_tracked,
-                    last_session=log if x == False else None)
-            # print(currently_tracked)
-            print(log)
+            log_infos = {
+                    "list_of_tracked_tasks": tracked,
+                    "currently_tracked_tasks": currently_tracked,
+                    }
+            if x == "done":
+                last_log = log_parser(
+                        **log_infos,
+                        last_session=last_log
+                        )
+            elif x == "file":
+                last_log = log_parser(
+                        **log_infos,
+                        last_session=file_load
+                        )
+            elif x == None:
+                 last_log = log_parser(
+                        **log_infos,
+                        last_session=None
+                        )
+            print(last_log)
             time.sleep(update_delay)
             os.system("clear")
-            x = False
-        with open(f"{time.strftime('%h-%j')}.log", "w") as day_log_file:
-            day_log_file.writelines(json.dumps(log))
-        print("log written")
+            x = "done"
+        
+        hour_log_file_path = f"log/{time.strftime('%Y')}/{time.strftime('%j')}"
+
+        try:
+            os.makedirs(hour_log_file_path)
+        except FileExistsError:
+            pass
+        with open(f"{hour_log_file_path}/{time.strftime('%H')}.log", "w") as hour_log_file:
+            hour_log_file.writelines(json.dumps(last_log))
+            print("log written")
+        # except
 
 if __name__ == "__main__":
     main()
